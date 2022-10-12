1785887238452b425dee16cef68a959debda2f5c
AlessandroVola <volaalessandro@gmail.com>
2022-10-12 16:52:38 +0200

    print text and length

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione velit laudantium assumenda iusto omnis? Eum ratione fugit cupiditate aspernatur exercitationem nostrum assumenda ipsam recusandae perferendis autem, laboriosam cum nesciunt aut.';
+
+    $length = strlen($text);
+
+
+
+
+?>
+
+
+<div>
+    <?= $text ?>
+</div>
+
+<div>
+    <p>Il testo è formato da <?= $length ?> parole </p>
+</div>
